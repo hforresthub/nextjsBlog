9222c7f33024047ac477da2f3e38d358f8a1bb7d
Hal <hgrforrest@gmail.com>
2022-02-25 17:02:51 -0500

    using Head and Image

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,29 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
+
+const YourComponent = () => (
+	<Image
+		src='/images/profile.png'
+		height={144}
+		width={144}
+		alt='your name'
+	/>
+)
 
 export default function FirstPost() {
 	return (
 		<>
+			<Head>
+				<title>First Post</title>
+			</Head>
 			<h1>First Post</h1>
 			<h2>
 				<Link href='/'>
 					<a className='classGoHere'>Back to homepage</a>
 				</Link>
 			</h2>
+			<YourComponent />
 		</>
 	)
 }
